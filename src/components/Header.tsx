ce7e4802ffa192cf2750e61a9090e33995f31188
OlgaBorzdyko <helga.borzdyko@gmail.com>
2025-07-17 19:06:41 +0400

    Added changes for svg files and Header, added LocationIcon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,13 @@
 import { AppBar, Toolbar, Typography } from '@mui/material'
 
+import LocationIcon from '@/assets/icons/LocationIcon.svg?react'
+
 const Header = () => {
   return (
     <>
       <AppBar color="transparent" elevation={0} position="static">
         <Toolbar sx={{ px: '72px', py: '10px' }}>
+          <LocationIcon style={{ width: 32, height: 32 }} />
           <Typography
             sx={{
               color: 'primary',
