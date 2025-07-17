55519ecce4e1c3440ac8d4c9024d5f54067fbd35
OlgaBorzdyko <helga.borzdyko@gmail.com>
2025-07-17 19:26:05 +0400

    hot fix for Header location

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,14 @@ const Header = () => {
   return (
     <>
       <AppBar color="transparent" elevation={0} position="static">
-        <Toolbar sx={{ px: '72px', py: '10px' }}>
+        <Toolbar
+          sx={{
+            px: '72px',
+            py: '12px',
+            minHeight: '32px !important',
+            borderBottom: '1px solid #E1E1E1'
+          }}
+        >
           <LocationIcon style={{ width: '12.67px', height: '12.67px' }} />
           <Typography
             sx={{
