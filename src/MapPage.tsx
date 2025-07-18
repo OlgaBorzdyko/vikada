e15c68b24aebf709ecb5041f94b733b8767a92c9
OlgaBorzdyko <helga.borzdyko@gmail.com>
2025-07-18 12:49:16 +0400

    Added breadcrumbs

--- src/MapPage.tsx
+++ src/MapPage.tsx
@@ -1,4 +1,4 @@
-import { Container } from '@mui/material'
+import { Breadcrumbs, Container, Link, Typography } from '@mui/material'
 
 import Header from './components/Header'
 import MapComponent from './components/map/MapComponent'
@@ -8,6 +8,12 @@ const MapPage = () => {
     <>
       <Container>
         <Header />
+        <Breadcrumbs aria-label="breadcrumb" separator="›">
+          <Link href="/">
+            <Typography color="text.secondary">Главная</Typography>
+          </Link>
+          <Typography color="text.secondary">Карты</Typography>
+        </Breadcrumbs>
         <MapComponent />
       </Container>
     </>
