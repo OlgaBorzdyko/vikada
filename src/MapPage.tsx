7c76001aa383314f3b82ff1283629833c9d49ea5
OlgaBorzdyko <helga.borzdyko@gmail.com>
2025-07-18 12:13:31 +0400

    Added MapComponent

--- src/MapPage.tsx
+++ src/MapPage.tsx
@@ -1,12 +1,14 @@
 import { Container } from '@mui/material'
 
 import Header from './components/Header'
+import MapComponent from './components/map/MapComponent'
 
 const MapPage = () => {
   return (
     <>
       <Container>
         <Header />
+        <MapComponent />
       </Container>
     </>
   )
---
+++ src/components/map/MapComponent.tsx
@@ -0,0 +1,33 @@
+import 'ol/ol.css'
+
+import TileLayer from 'ol/layer/Tile'
+import Map from 'ol/Map'
+import { fromLonLat } from 'ol/proj'
+import OSM from 'ol/source/OSM'
+import View from 'ol/View'
+import { useEffect, useRef } from 'react'
+
+const MapComponent = () => {
+  const mapRef = useRef<HTMLDivElement>(null)
+  const mapInstanceRef = useRef<Map | null>(null)
+
+  useEffect(() => {
+    if (!mapRef.current || mapInstanceRef.current) return
+    const map = new Map({
+      target: mapRef.current,
+      layers: [
+        new TileLayer({
+          source: new OSM()
+        })
+      ],
+      view: new View({
+        center: fromLonLat([37.6173, 55.7558]),
+        zoom: 10
+      })
+    })
+    mapInstanceRef.current = map
+  }, [])
+  return <div ref={mapRef} style={{ width: '100%', height: '750px' }} />
+}
+
+export default MapComponent
