440d7c9656e1b5690cff35e8e3907f5c06cc9871
OlgaBorzdyko <helga.borzdyko@gmail.com>
2025-07-22 15:10:50 +0400

    Added items to TagMainInfo

--- src/components/map-page/tagInfo/TagMainInfo.tsx
+++ src/components/map-page/tagInfo/TagMainInfo.tsx
@@ -1,5 +1,33 @@
+import { Box, Typography } from '@mui/material'
+
+import { TagInfoArray } from './TagInfoArray'
+
+type ItemData = {
+  title: string
+  description: string
+  img: string
+  address: string
+  webSite: string
+  openingHours: string
+}
+
 const TagMainInfo = () => {
-  return <div>Здесь информация по тегу</div>
+  return (
+    <div>
+      {TagInfoArray.map((item: ItemData, index: number) => {
+        return (
+          <Box key={index}>
+            <Typography variant="h2">{item.title}</Typography>
+            <Typography variant="body1">{item.description}</Typography>
+            {item.img}
+            {item.address}
+            {item.webSite}
+            {item.openingHours}
+          </Box>
+        )
+      })}
+    </div>
+  )
 }
 
 export default TagMainInfo
