640798d0dbd849a4ac7e1d181b0d14b5a926076b
OlgaBorzdyko <helga.borzdyko@gmail.com>
2025-07-23 14:47:09 +0400

    Added background to auth page

--- src/components/authorization/AuthorizationPage.tsx
+++ src/components/authorization/AuthorizationPage.tsx
@@ -1,4 +1,16 @@
+import { Box, Typography, useTheme } from '@mui/material'
+
 const AuthorizationPage = () => {
-  return <div>Страница входа</div>
+  const theme = useTheme()
+  return (
+    <Box
+      sx={{
+        background: `linear-gradient(to left, white 50%, ${theme.palette.primary.main} 50%)`,
+        height: '100vh'
+      }}
+    >
+      <Typography>Авторизация</Typography>
+    </Box>
+  )
 }
 export default AuthorizationPage
