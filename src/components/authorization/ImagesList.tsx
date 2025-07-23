3d397823c78483ebdd4880f63b9951f80918b5be
OlgaBorzdyko <helga.borzdyko@gmail.com>
2025-07-23 16:54:10 +0400

    Fixed mui styles for image list

--- src/components/authorization/ImagesList.tsx
+++ src/components/authorization/ImagesList.tsx
@@ -3,21 +3,18 @@ import ImageListItem from '@mui/material/ImageListItem'
 
 import { itemData } from './imagesArray'
 
-function srcset(image: string, size: number, rows = 1, cols = 1) {
-  return {
-    src: `${image}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format`,
-    srcSet: `${image}?w=${size * cols}&h=${
-      size * rows
-    }&fit=crop&auto=format&dpr=2 2x`
-  }
-}
-
 const ImagesList = () => {
   return (
     <ImageList
       cols={3}
       rowHeight={183}
-      sx={{ width: 'auto', height: 'auto' }}
+      sx={{
+        width: 'auto',
+        height: 'auto',
+        '& .MuiImageListItem-root': {
+          padding: '12px'
+        }
+      }}
       variant="quilted"
     >
       {itemData.map((item) => (
@@ -27,9 +24,15 @@ const ImagesList = () => {
           rows={item.rows || 1}
         >
           <img
-            {...srcset(item.img, 121, item.rows, item.cols)}
             alt={item.title}
             loading="lazy"
+            src={item.img}
+            style={{
+              width: '100%',
+              height: '100%',
+              objectFit: 'contain',
+              display: 'block'
+            }}
           />
         </ImageListItem>
       ))}
