925019c274865533a8046e992771d8d676d29879
OlgaBorzdyko <helga.borzdyko@gmail.com>
2025-07-25 18:52:32 +0400

    Added registration form logic

--- src/components/authorization/RegistrationForm.tsx
+++ src/components/authorization/RegistrationForm.tsx
@@ -1,17 +1,65 @@
 import { Box, Button, TextField } from '@mui/material'
+import { FC, useState } from 'react'
 
-const RegistrationForm = () => {
+interface RegistrationFormData {
+  name: string
+  lastName: string
+  email: string
+  password: string
+}
+
+const RegistrationForm: FC<RegistrationFormData> = () => {
+  const [userFormData, setUserFormData] = useState<Partial<FormData>>({})
+
+  const initialFormState = {
+    name: '',
+    lastName: '',
+    email: '',
+    password: ''
+  }
+
+  const formData = {
+    ...initialFormState,
+    ...userFormData
+  }
+  const onHandleChange = (value: string, key: string) => {
+    setUserFormData({ ...formData, [key]: value })
+  }
+
+  const onHandleSubmit = () => {
+    console.log(userFormData)
+  }
   return (
     <Box>
       <Box>
-        <TextField label="Имя" variant="outlined" />
-        <TextField label="Фамилия" variant="outlined" />
-        <TextField label="Почта" variant="outlined" />
-        <TextField label="● ● ● ● ● ● ● ●" variant="outlined" />
-        <TextField label="● ● ● ● ● ● ● ●" variant="outlined" />
+        <TextField
+          label="Имя"
+          onChange={(e) => onHandleChange(e.currentTarget.value, 'name')}
+          value={formData.name}
+          variant="outlined"
+        />
+        <TextField
+          label="Фамилия"
+          onChange={(e) => onHandleChange(e.currentTarget.value, 'lastName')}
+          value={formData.lastName}
+          variant="outlined"
+        />
+        <TextField
+          label="Почта"
+          onChange={(e) => onHandleChange(e.currentTarget.value, 'email')}
+          value={formData.email}
+          variant="outlined"
+        />
+        <TextField
+          key="password"
+          label="● ● ● ● ● ● ● ●"
+          onChange={(e) => onHandleChange(e.currentTarget.value, 'password')}
+          value={formData.password}
+          variant="outlined"
+        />
       </Box>
       <Box>
-        <Button>Зарегистрировать</Button>
+        <Button onClick={onHandleSubmit}>Зарегистрировать</Button>
       </Box>
     </Box>
   )
