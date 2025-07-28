19321b5b3d911eabd432d5a3a0370ea1a9657a80
OlgaBorzdyko <helga.borzdyko@gmail.com>
2025-07-28 15:34:42 +0400

    Added registrationSchema

---
+++ src/components/registration/RegistrationSchema.tsx
@@ -0,0 +1,22 @@
+import * as yup from 'yup'
+import yupPassword from 'yup-password'
+
+yupPassword(yup)
+
+export const registrationSchema = yup.object().shape({
+  email: yup.string().email('Неверный email').required('Обязательное поле'),
+  password: yup
+    .string()
+    .minUppercase(1, 'Пароль должен содержать минимум одну заглавную букву')
+    .min(8, 'Пароль должен содержать минимум 8 символов')
+    .minNumbers(1, 'Пароль должен содержать минимум 1 цифру')
+    .minSymbols(
+      1,
+      'Пароль должен содержать минимум 1 спецсимвол (например, !@#$%^&*)'
+    )
+    .maxRepeating(
+      3,
+      'Пароль должен содержать максимум 3 одинаковых символа подряд'
+    )
+    .required('Обязательное поле')
+})
