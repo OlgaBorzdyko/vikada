153eea79003374b31d12b0147b2f0dd16cd5e1a0
OlgaBorzdyko <helga.borzdyko@gmail.com>
2025-07-29 15:22:58 +0400

    hot fix

--- src/components/registration/RegistrationSchema.tsx
+++ src/components/registration/RegistrationSchema.tsx
@@ -7,16 +7,16 @@ yupPassword(yup)
 
 export const passwordSchema = passwordRules.reduce(
   (schema, rule) => rule.yup(schema),
-  yup.string().required('Введите пароль')
+  yup.string().required('*Введите пароль')
 )
 
 export const registrationSchema = yup.object().shape({
-  email: yup.string().email('Некорректный email').required('Введите почту'),
-  name: yup.string().required('Введите имя'),
-  lastName: yup.string().required('Введите фамилию'),
+  email: yup.string().email('Некорректный email').required('*Введите почту'),
+  name: yup.string().required('*Введите имя'),
+  lastName: yup.string().required('*Введите фамилию'),
   password: passwordSchema,
   confirmPassword: yup
     .string()
     .oneOf([yup.ref('password')], 'Пароли не совпадают')
-    .required('Повторите пароль')
+    .required('*Повторите пароль')
 })
