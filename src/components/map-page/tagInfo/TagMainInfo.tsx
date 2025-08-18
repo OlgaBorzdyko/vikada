1c8032b454f37c4fa1fcb1d91cb3ded7d1bff615
OlgaBorzdyko <helga.borzdyko@gmail.com>
2025-08-18 20:50:16 +0400

    Added schedule for tag main info

--- src/components/map-page/tagInfo/TagMainInfo.tsx
+++ src/components/map-page/tagInfo/TagMainInfo.tsx
@@ -3,9 +3,29 @@ import { Box, Typography } from '@mui/material'
 import { useMapStore } from '../../../store/MapStore'
 import ImagesSwiper from './ImagesSwiper'
 
+type DaySchedule = {
+  from: string
+  to: string
+}
+
+const getDayName = (dayNumber: number) => {
+  const days: Record<number, string> = {
+    1: 'Понедельник',
+    2: 'Вторник',
+    3: 'Среда',
+    4: 'Четверг',
+    5: 'Пятница',
+    6: 'Суббота',
+    7: 'Воскресенье'
+  }
+  return days[dayNumber] ?? '-'
+}
+
 const TagMainInfo = () => {
   const selectedObject = useMapStore((state) => state.selectedObject)
-  console.log('selectedObject', selectedObject)
+  const scheduleArray = Object.entries(
+    selectedObject?.workingSchedule ?? ({} as [string, DaySchedule][])
+  )
   return (
     <div>
       <Box
@@ -27,9 +47,18 @@ const TagMainInfo = () => {
             </Typography>
           </Box>
           <Typography variant="body1">{selectedObject?.webSite}</Typography>
-          <Typography variant="body1">
-            {/*График работы: {selectedObject.openingHours}*/}
-          </Typography>
+          <Box gap={2}>
+            {scheduleArray.map(([day, hours]) => (
+              <Box display="flex" flexDirection="row" key={day}>
+                <Typography>{getDayName(Number(day))}</Typography>
+                <Typography>
+                  {hours?.from && hours?.to
+                    ? `${hours.from} - ${hours.to}`
+                    : '-'}
+                </Typography>
+              </Box>
+            ))}
+          </Box>
         </Box>
       </Box>
     </div>
