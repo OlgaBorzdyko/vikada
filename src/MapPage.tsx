c8f2e4b57467cef68d8a6d38b1dd0f4cc41f8ca3
OlgaBorzdyko <helga.borzdyko@gmail.com>
2025-09-10 16:44:19 +0400

    Changed map ui for pc and mobile

--- src/MapPage.tsx
+++ src/MapPage.tsx
@@ -2,56 +2,80 @@ import {
   AppBar,
   Box,
   Breadcrumbs,
-  Container,
   Link,
-  Typography
+  Typography,
+  useTheme
 } from '@mui/material'
 import { useState } from 'react'
 
-import HeaderLocationBar from './components/header/HeaderLocationBar'
-import HeaderNavigation from './components/header/HeaderNavigation'
+import Header from './components/header/Header'
 import MapComponent from './components/map-page/map/MapComponent'
 import SearchComponent from './components/map-page/search/SearchComponent'
 import TagInfoComponent from './components/map-page/tagInfo/TagInfoComponent'
-import ProfileButton from './components/profile/ProfileButton'
 
 const MapPage = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false)
+  const theme = useTheme()
+
   return (
-    <div style={{ position: 'relative' }}>
-      <Container>
-        <AppBar color="transparent" elevation={0} position="static">
-          <HeaderLocationBar />
-          <HeaderNavigation>
-            <ProfileButton />
-          </HeaderNavigation>
-        </AppBar>
-        <Breadcrumbs
-          aria-label="breadcrumb"
-          separator="›"
-          sx={{ paddingBottom: '24px' }}
+    <Box sx={{ position: 'relative' }}>
+      {/* header */}
+      <AppBar
+        color="transparent"
+        elevation={0}
+        position="static"
+        sx={{
+          px: theme.fn.clampVW(72, 16, 72),
+          pt: theme.fn.clampVW(24, 12, 32)
+        }}
+      >
+        <Header />
+      </AppBar>
+
+      {/* breadcrumbs */}
+      <Breadcrumbs
+        aria-label="breadcrumb"
+        separator="›"
+        sx={{
+          px: theme.fn.clampVW(72, 16, 72),
+          pb: theme.fn.clampVW(24, 12, 32)
+        }}
+      >
+        <Link href="/">
+          <Typography color="text.secondary">Главная</Typography>
+        </Link>
+        <Typography color="text.secondary">Карты</Typography>
+      </Breadcrumbs>
+
+      {/* map */}
+      <Box
+        sx={{
+          position: 'relative',
+          width: '100%',
+          height: {
+            xs: '100vh',
+            md: `calc(100vh - ${theme.fn.clampVW(200, 160, 240)})`
+          },
+          borderRadius: { xs: 0, md: theme.fn.clampVW(12, 0, 12) },
+          overflow: 'hidden'
+        }}
+      >
+        <Box
+          sx={{
+            position: 'absolute',
+            top: theme.fn.clampVW(16, 8, 24),
+            left: theme.fn.clampVW(16, 8, 24),
+            zIndex: 1000
+          }}
         >
-          <Link href="/">
-            <Typography color="text.secondary">Главная</Typography>
-          </Link>
-          <Typography color="text.secondary">Карты</Typography>
-        </Breadcrumbs>
-        <div style={{ position: 'relative' }}>
-          <Box
-            style={{
-              position: 'absolute',
-              top: 16,
-              left: 16,
-              zIndex: 1000
-            }}
-          >
-            <SearchComponent />
-          </Box>
-          <MapComponent setIsOpen={setIsOpen} />
-          <TagInfoComponent isOpen={isOpen} setIsOpen={setIsOpen} />
-        </div>
-      </Container>
-    </div>
+          <SearchComponent />
+        </Box>
+
+        <MapComponent setIsOpen={setIsOpen} />
+        <TagInfoComponent isOpen={isOpen} setIsOpen={setIsOpen} />
+      </Box>
+    </Box>
   )
 }
+
 export default MapPage
